d310ea2f9f1c042efa5aeda0b35708313cbf4732
Greg P <88697977+v-9x@users.noreply.github.com>
2025-04-04 11:46:57 -0700

    Clean up code

--- lab1/grade.cpp
+++ lab1/grade.cpp
@@ -1,24 +1,24 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
-int main() {
-  int i = 0;
-  int st_num = 0;
-  double total = 0;
-  vector<double> num_vec;
-
-  while (st_num < 2) {
+int numOfStudents(int n) {
+  while (n < 2) {
     cout << "Please enter the number of students: ";
-    while (!(cin >> st_num) || cin.get() != '\n') {
+    while (!(cin >> n) || cin.get() != '\n') {
       cerr << "Please enter a valid number of students: ";
       cin.clear();
       cin.ignore(100, '\n');
     }
   }
+  return n;
+}
 
-  while (i < st_num) {
+vector<double> getGrades(vector<double> v, int n) {
+  int i = 0;
+  while (i < n) {
     double grade;
     cout << "Enter score for student " << i + 1 << ": ";
     while (!(cin >> grade) || cin.get() != '\n') {
@@ -29,15 +29,24 @@ int main() {
     if (grade < 0 || grade > 100) {
       cerr << "Score must be 0-100." << endl;
     } else {
-      total += grade;
-      num_vec.push_back(grade);
+      v.push_back(grade);
       i++;
     }
   }
+  return v;
+}
 
-  double avg = total / st_num;
+int main() {
+  vector<double> num_vec;
+  int i = 0;
+  int st_num = 0;
+  st_num = numOfStudents(st_num);
+  num_vec = getGrades(num_vec, st_num);
+  // we use functions to calculate the elements in the double vector
+  double total = accumulate(num_vec.begin(), num_vec.end(), 0);
   double max_val = *max_element(num_vec.begin(), num_vec.end());
   double min_val = *min_element(num_vec.begin(), num_vec.end());
+  double avg = total / st_num;
 
   cout << "Average score is: " << avg << endl;
   cout << "Min score is: " << min_val << endl;

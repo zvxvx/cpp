8e53386f1fc9cd712745991478b82d8c4bd88b21
Greg P <88697977+v-9x@users.noreply.github.com>
2025-05-06 20:39:59 -0700

    Update lab

--- lab4/Graph.cpp
+++ lab4/Graph.cpp
@@ -7,22 +7,43 @@
 
 using namespace std;
 bool Graph::loadCityInformation(const char* fileName) {
-  ifstream file("Terrain.obj");
+  ifstream file(fileName);
   if (!file.is_open()) {
-    cout << "Error opening file!";
-  } else {
-    string line;
+    cerr << "Error opening file!";
+    return false;
+  }
+  string line;
+
+  while (getline(file, line)) {
     istringstream iss(line);
+
     char p;
     double x, y, z;
-    while (getline(file, line)) {
-      if (line.find("p") != -1) {
-        if (iss >> p >> x >> y >> z) {
-          cityCoordinates.push_back(Point3D(x, y, z));
-        }
-      } else if (line.find("c") != -1) {
-        // cityIndices.push_back();
+    int d, e, f;
+
+    if (line[0] == 'p') {
+      if (iss >> p >> x >> y >> z) {
+        cityCoordinates.push_back(Point3D(x, y, z));
+      }
+    } else if (line[0] == 'c') {
+      if (iss >> p >> d >> e >> f) {
+        cityIndices.push_back(d);
+        cityIndices.push_back(e);
+        cityIndices.push_back(f);
       }
     }
   }
+  // for (int i = 0; i < cityCoordinates.size(); i++) {
+  //   cout << cityCoordinates[i].getX() << " " << cityCoordinates[i].getY() <<
+  //   " "
+  //        << cityCoordinates[i].getZ() << endl;
+  // }
+  // for (int i = 0; i < cityIndices.size(); i++) {
+  //   cout << cityIndices[i] << " ";
+  // }
+  return true;
+}
+
+void Graph::Generate() {
+
 }
